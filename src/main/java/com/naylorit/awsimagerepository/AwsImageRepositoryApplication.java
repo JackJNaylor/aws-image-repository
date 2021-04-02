130604c0791d42cd65fb66b54b9c94dbeff1b4c7
Jack Naylor <jacknaylor@rogers.com>
2021-04-02 16:41:46 -0400

    Initial commit

---
+++ src/main/java/com/naylorit/awsimagerepository/AwsImageRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.naylorit.awsimagerepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naylorit/awsimagerepository/AwsImageRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naylorit.awsimagerepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
