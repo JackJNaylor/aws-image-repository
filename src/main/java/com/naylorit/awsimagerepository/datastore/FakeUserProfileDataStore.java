77676c366332e716baf2b880f384683cc676af90
Jack Naylor <jacknaylor@rogers.com>
2021-04-06 11:59:35 -0400

    Upload Profile Image to S3

--- src/main/java/com/naylorit/awsimagerepository/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/naylorit/awsimagerepository/datastore/FakeUserProfileDataStore.java
@@ -13,8 +13,8 @@ public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "jacknaylor", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "lucynaylor", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("51db8d3a-ee56-4d49-a1b4-901dbe36fb3d"), "franklinsmart", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("c73f8900-9300-48c7-9fbd-3b0618441ff7"), "natashabells", null));
 
     }
 
--- src/main/java/com/naylorit/awsimagerepository/user/UserProfileService.java
+++ src/main/java/com/naylorit/awsimagerepository/user/UserProfileService.java
@@ -1,20 +1,26 @@
 package com.naylorit.awsimagerepository.user;
 
+import com.naylorit.awsimagerepository.bucket.BucketName;
+import com.naylorit.awsimagerepository.filestore.FileStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
+
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     List<UserProfile> getUserProfiles() {
@@ -23,9 +29,57 @@ public class UserProfileService {
 
     void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
         // 1. Check if image is not empty
+        isFileEmpty(file);
+
         // 2. Check if file is an image
+        isImage(file);
+
         // 3. Check whether the user exists in database
+        UserProfile user = getUserProfileOrThrow(userProfileId);
+
         // 4. Grab some metadata from file if any
+        Map<String, String> metadata = extractMetadata(file);
+
         // 5. Store the image in s3 bucket and update database (userProfileImageLink) with new s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String filename = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
+
+        try {
+            fileStore.save(path, filename, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    private Map<String, String> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return metadata;
+    }
+
+    private UserProfile getUserProfileOrThrow(UUID userProfileId) {
+        UserProfile user = userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User Profile %s not found", userProfileId)));
+        return user;
+    }
+
+    private void isImage(MultipartFile file) {
+        if (!Arrays.asList(ContentType.IMAGE_JPEG.getMimeType(),
+                ContentType.IMAGE_PNG.getMimeType(),
+                ContentType.IMAGE_GIF.getMimeType()).contains(file.getContentType())){
+            throw new IllegalStateException("File Must Be An Image [" + file.getContentType() + "]");
+        }
+    }
+
+    private void isFileEmpty(MultipartFile file) {
+        if (file.isEmpty()){
+            throw new IllegalStateException("Empty File [" + file.getSize() + "]");
+        }
     }
 }
